2e783b1ed8511419b761011037921e10db5d5de0
Ivan-b01 <ivanftbl01@gmail.com>
2024-10-07 17:55:24 +0530

    c3

--- numberpad.py
+++ numberpad.py
@@ -8,10 +8,7 @@ for i in range(4):
     root.rowconfigure(i, weight=1, minsize=50)
 for i in range(4): 
     for j in range(3): 
-        frame = Frame(
-            master=root,
-            releif=SUNKEN,
-            borderwidth=1
+        frame = Frame(master=root,releif=SUNKEN, borderwidth=1,
             bg="#456f2f"
         )    
         frame.grid(row=i, column=j, sticky='nsew')
